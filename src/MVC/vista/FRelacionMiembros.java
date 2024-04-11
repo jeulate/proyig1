ac8da9fd00c262618d1f71d0e7a54c62d4c2a9f8
Gerardo Vargas <vargasgerardocopaz@gmail.com>
2024-04-11 05:02:30 -0400

    Relaciones Miembros

--- src/MVC/vista/FRelacionMiembros.java
+++ src/MVC/vista/FRelacionMiembros.java
@@ -6,6 +6,7 @@ import MVC.modelo.entity.Miembros;
 import MVC.modelo.entity.Relaciones;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.List;
@@ -21,12 +22,15 @@ public class FRelacionMiembros {
     private JComboBox comboMiembro2;
     private JPanel PPRelacion;
     private JButton btnMostrar;
-    private JTable table1;
+    private JTable tablaDatos;
+    private JPanel ppdatos;
 
     List<Miembros> miembros1;
     List<Miembros> miembros2;
     DefaultComboBoxModel<Miembros> modeloComboMiembro1;
     DefaultComboBoxModel<Miembros> modeloComboMiembro2;
+
+    private DefaultTableModel modeloTabla;
 
     public FRelacionMiembros() {
         miembrosController = new MiembrosController();
@@ -34,6 +38,13 @@ public class FRelacionMiembros {
 
         cargarMiembro1();
         cargarMiembro2();
+
+        String[] columnas = {"ID", "Miembro 1", "Miembro 2","Relacion"};
+        modeloTabla = new DefaultTableModel(columnas,0);
+        tablaDatos = new JTable(modeloTabla);
+
+        JScrollPane scrollPane =new JScrollPane(tablaDatos);
+        ppdatos.add(scrollPane);
 
         btnGuardar.addActionListener(new ActionListener() {
             @Override
@@ -52,6 +63,27 @@ public class FRelacionMiembros {
 
                 relacionesMiembrosController.registrarNuevaRelacionMiembro(relaciones);
 
+            }
+        });
+        btnMostrar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                List<Relaciones> relaciones =  relacionesMiembrosController.obtenerRelaciones();
+                List<Miembros> miembros = miembrosController.obtenerMiembros();
+
+                limpiarTabla(modeloTabla);
+//                    modeloTabla = new DefaultTableModel(columnas,0);
+
+                for (Relaciones relacion : relaciones) {
+
+                    modeloTabla.addRow(new Object[]{
+                            getNombreMiembro(miembros, relacion.getMiembro1_id()),
+                            getNombreMiembro(miembros, relacion.getMiembro2_id()),
+                            relacion.getTipo_relacion()
+                    });
+                }
+
+                modeloTabla.fireTableDataChanged();
             }
         });
     }
@@ -79,6 +111,29 @@ public class FRelacionMiembros {
         }
 
         comboMiembro2.setModel(modeloComboMiembro2);
+    }
+
+    private void limpiarTabla(DefaultTableModel modelo) {
+        // Se elimina el contenido de la tabla fila por fila desde arriba
+        while (modelo.getRowCount() > 0) {
+            modelo.removeRow(0);
+        }
+    }
+
+    private String getNombreMiembro(List<Miembros> miembros, int idMiembro) {
+        // Se elimina el contenido de la tabla fila por fila desde arriba
+        Miembros miembro = null;
+        for (int iMiembro = 0; iMiembro < miembros.size(); iMiembro++) {
+            if(miembros.get(iMiembro).getId() == idMiembro) {
+                miembro = miembros.get(iMiembro);
+                break;
+            }
+        }
+        if( miembro == null) {
+            return "";
+        }
+
+        return miembro.toString();
     }
 
     public static void main(String[] args) {
