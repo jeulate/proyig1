9042156dbb805a3483b83320c04f6833d0896210
Juan Eulate <juan_a_eulate@hotmail.com>
2024-04-11 01:16:08 -0400

    act1

--- src/MVC/modelo/Asignaciones.java
+++ src/MVC/modelo/Asignaciones.java
@@ -2,49 +2,48 @@ package MVC.modelo;
 
 import java.util.Date;
 
-public class Asignaciones extends Ministerios {
-    private int IdMiembro;
-    private int idMinisterio;
-    private Date finicio;
-    private Date ffin;
+public class Asignaciones{
+    private int miembro_id;
+    private int ministerio_id;
+    private Date fecha_inicio;
+    private Date fecha_fin;
 
-    public Asignaciones(String nombre, String descripcion, int idMiembro, int idMinisterio, Date finicio, Date ffin) {
-        super(nombre, descripcion);
-        IdMiembro = idMiembro;
-        this.idMinisterio = idMinisterio;
-        this.finicio = finicio;
-        this.ffin = ffin;
+    public Asignaciones(int miembro_id, int ministerio_id, Date fecha_inicio, Date fecha_fin) {
+        this.miembro_id = miembro_id;
+        this.ministerio_id = ministerio_id;
+        this.fecha_inicio = fecha_inicio;
+        this.fecha_fin = fecha_fin;
     }
 
-    public int getIdMiembro() {
-        return IdMiembro;
+    public int getMiembro_id() {
+        return miembro_id;
     }
 
-    public void setIdMiembro(int idMiembro) {
-        IdMiembro = idMiembro;
+    public void setMiembro_id(int miembro_id) {
+        this.miembro_id = miembro_id;
     }
 
-    public int getIdMinisterio() {
-        return idMinisterio;
+    public int getMinisterio_id() {
+        return ministerio_id;
     }
 
-    public void setIdMinisterio(int idMinisterio) {
-        this.idMinisterio = idMinisterio;
+    public void setMinisterio_id(int ministerio_id) {
+        this.ministerio_id = ministerio_id;
     }
 
-    public Date getFinicio() {
-        return finicio;
+    public Date getFecha_inicio() {
+        return fecha_inicio;
     }
 
-    public void setFinicio(Date finicio) {
-        this.finicio = finicio;
+    public void setFecha_inicio(Date fecha_inicio) {
+        this.fecha_inicio = fecha_inicio;
     }
 
-    public Date getFfin() {
-        return ffin;
+    public Date getFecha_fin() {
+        return fecha_fin;
     }
 
-    public void setFfin(Date ffin) {
-        this.ffin = ffin;
+    public void setFecha_fin(Date fecha_fin) {
+        this.fecha_fin = fecha_fin;
     }
 }
